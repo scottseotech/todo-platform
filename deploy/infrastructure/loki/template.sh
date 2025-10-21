3c631a20b9717aa7c85b9bbb34ee7d8e4cf69c86
7onetella <7onetella@gmail.com>
2025-10-21 16:27:11 -0400

    deploying fluent-bit and loki

---
+++ deploy/infrastructure/loki/template.sh
@@ -0,0 +1,28 @@
+#!/bin/bash -x
+
+APP_NAME=loki
+NAMESPACE=logging
+
+helm repo add grafana https://grafana.github.io/helm-charts
+
+helm repo update
+
+helm show values grafana/loki > default-values.yaml 
+
+helm template loki grafana/loki --namespace $NAMESPACE -f ./custom-values.yaml > all-resources.yaml
+
+kustomize edit set namespace $NAMESPACE
+
+git add .
+
+git commit -m "updating ${APP_NAME}"
+
+git push origin main
+
+
+# Manual step while setting up loki datasource
+#
+# In Add Datasource
+# Set up the connection URL as http://loki.logging.svc.cluster.local:3100
+# HTTP Headers > Add Header > X-Scope-OrgID: todo
+# The header is not necessary if we set loki.auth_enabled to false
