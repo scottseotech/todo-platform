eee7697b7df2a844910a1e2806f711a672d4b4e6
7onetella <7onetella@gmail.com>
2025-10-28 14:38:32 -0400

    renaming grafana-tempo to tempo

--- deploy/infrastructure/tempo/helm-template.sh
+++ deploy/infrastructure/tempo/helm-template.sh
@@ -9,4 +9,24 @@ helm repo update
 
 helm show values grafana/tempo > default-values.yaml
 
-helm template grafana-tempo --namespace observability grafana/tempo -f ./custom-values.yaml > all-resources.yaml
+helm template tempo --namespace observability grafana/tempo -f ./custom-values.yaml > all-resources.yaml
+
+
+
+# grafana:
+#   additionalDataSources:
+#     - name: Tempo
+#       type: tempo
+#       access: proxy
+#       url: http://tempo.observability.svc.cluster.local:3100
+#       isDefault: false
+#       jsonData:
+#         nodeGraph:
+#           enabled: true
+#         tracesToLogs:
+#           # If you also have Loki, wire correlation:
+#           datasourceUid: loki
+#           spanStartTimeShift: '1h'
+#           spanEndTimeShift: '1h'
+#         tracesToMetrics:
+#           datasourceUid: prometheus
