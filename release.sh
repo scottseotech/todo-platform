8c0d8cc10ce4e11bfbaa2752e03b272eda363362
7onetella <7onetella@gmail.com>
2025-11-03 18:52:18 -0500

    chore: updating release script

--- release.sh
+++ release.sh
@@ -3,6 +3,14 @@
 # Install svu from https://github.com/caarlos0/svu
 # Install gum from https://github.com/charmbracelet/gum
 
+# chore: foo	Nothing
+# fix: fixed something	Patch
+# feat: added new button to do X	Minor
+# fix: fixed thing xyz
+# BREAKING CHANGE: this will break users because of blah	Major
+# fix!: fixed something	Major
+# feat!: added blah	Major
+
 part=$(gum choose next patch minor major)
 
 version=$(svu ${part})
