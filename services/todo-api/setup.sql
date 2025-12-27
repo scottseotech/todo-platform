c919bff6ee59bd464987dcf520c1ab1aa6613f2f
7onetella <7onetella@gmail.com>
2025-12-27 11:06:19 -0500

    fixing postgres permissions

--- services/todo-api/setup.sql
+++ services/todo-api/setup.sql
@@ -1,4 +1,9 @@
+-- CNPG for some reason is creating this user with different password than what is specified in secret
+ALTER ROLE todo_db_admin WITH PASSWORD 'secure_password_here';
+
+-- If this schema is created by postgres user, we need to change its owner to todo_db_admin
 CREATE SCHEMA todo;
+ALTER SCHEMA todo OWNER TO todo_db_admin;
 
 -- We need to specify the search_path to todo
 ALTER USER todo_db_admin SET search_path TO todo;
