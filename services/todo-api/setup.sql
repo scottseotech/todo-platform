adf21e2d9c3a33098f151c0bb5fcecb5f8e30f36
7onetella <7onetella@gmail.com>
2025-12-29 20:47:27 -0500

    fixing setup.sql

--- services/todo-api/setup.sql
+++ services/todo-api/setup.sql
@@ -1,6 +1,9 @@
 -- CNPG for some reason is creating this user with different password than what is specified in secret
 ALTER ROLE todo_db_admin WITH PASSWORD 'secure_password_here';
 
+-- Connect to the todo_db database
+\c todo_db;
+
 -- If this schema is created by postgres user, we need to change its owner to todo_db_admin
 CREATE SCHEMA todo;
 ALTER SCHEMA todo OWNER TO todo_db_admin;
